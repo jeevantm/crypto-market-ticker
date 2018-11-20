bf054133f55f3add6c06c7250a0dfb6fc7dea789
Jeevan <jeevan.tm@gwynniebee.com>
2018-11-20 23:34:13 +0530

    coinmarketcap update

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def max_value(inputlist):
 
 def koinex_ticker(data):
     koinex_inr = []
-    # koinex_inr_stats = []
+
     koinex_coin_fetch_list = data["koinex_coin_fetch_list"]
 
     result = requests.get('https://koinex.in/api/ticker').json()
@@ -48,16 +48,33 @@ def wazirx_ticker(data):
     return wazirx_inr
 
 
-def build_tweet_body(koinex_inr, wazirx_inr):
+def coinmarketcap_ticker(data):
+    coinmarketcap_inr = []
+    coinmarketcap_coin_fetch_list = data["coinmarketcap_coin_fetch_list"]
+    coinmarketcap_secret_token = data["coinmarketcap_secret_token"]
+
+    result = requests.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol='+coinmarketcap_coin_fetch_list+'&convert=INR', headers={"X-CMC_PRO_API_KEY":coinmarketcap_secret_token}).json()
+
+    for coin in result["data"]:
+        coinmarketcap_inr.append([coin, '{0:.1f}'.format(result["data"][coin]["quote"]["INR"]["price"])])
+
+    return coinmarketcap_inr
+
+
+def build_tweet_body(koinex_inr, wazirx_inr, coinmarketcap_inr):
     body = "\nKOINEX (@koinexindia)\n"
     for kcoin in sorted(koinex_inr):
-        body = body + str(kcoin[0]) + " : " + str(kcoin[1]) + "\n"
+        body = body + str(kcoin[0]) + ":" + str(kcoin[1]) + "\n"
 
     body = body + "\nWAZIRX (@WazirXIndia)\n"
     for wcoin in sorted(wazirx_inr):
-        body = body + str(wcoin[0]) + " : " + str(wcoin[1]) + "\n"
+        body = body + str(wcoin[0]) + ":" + str(wcoin[1]) + "\n"
 
-    body = body + "\n#cryptocurrency #blockchain"
+    body = body + "\nCOINMARKETCAP (@CoinMarketCap)\n"
+    for ccoin in sorted(coinmarketcap_inr):
+        body = body + str(ccoin[0]) + ":" + str(ccoin[1]) + "\n"
+
+    # body = body + "\n#cryptocurrency #blockchain"
 
     return body
 
@@ -69,9 +86,10 @@ def main():
 
     koinex_inr = koinex_ticker(data)
     wazirx_inr = wazirx_ticker(data)
+    coinmarketcap_inr = coinmarketcap_ticker(data)
 
-    body = build_tweet_body(koinex_inr, wazirx_inr)
-    header = "\n#Crypto Market Update in INR\n"
+    body = build_tweet_body(koinex_inr, wazirx_inr, coinmarketcap_inr)
+    header = "\n#Crypto Market Update (INR)\n"
 
     tweet = header + body
 
